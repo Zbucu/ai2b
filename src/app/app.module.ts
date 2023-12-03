d915d48dabbf8999ea46524ac2ddc1f128022785
Szymon Buckowski <sbuckowski1@gmail.com>
2023-12-03 18:18:29 +0100

    lab-b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { AppComponent } from './app.component';
 import {HomeComponent} from "./home/home.component";
 import {provideRouter, RouterModule} from "@angular/router";
 import routeConfig from "./routes";
+import { RandomComponent } from './random/random.component';
+import { ListComponent } from './list/list.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
+    RandomComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
     HomeComponent,
     RouterModule,
+    FormsModule
   ],
   providers: [provideRouter(routeConfig)],
   bootstrap: [AppComponent]
